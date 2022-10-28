ab303a30aacf333b741d74ff44a459d43ebcace4
Amit Ranjan <amit@Amits-MacBook-Air.local>
2022-10-28 22:59:14 +0530

    assignment level 2 JS

--- block-BJaabd/code/index.js
+++ block-BJaabd/code/index.js
@@ -10,7 +10,22 @@ Write a program that asks the user his/her age and check for the following condi
 - `if` the age is greater than 55 then print the message " You are too old to participate in the marthon".
 👇
 */
-
+let age = prompt("Enter your age : ")
+if (age>=12 && age<=55) {
+  alert("You can participate in the marathon");
+}
+else if(age>=4 && age<=11) {
+  alert("You are too young to participate in the marathon")
+}
+else if(age<4) {
+  alert("Hey Kiddo! Can you walk ?")
+}
+else if (age>55) {
+  alert("You are too old to participate in the marahton")
+}
+else {
+  alert("invalid input")
+}
 // Loops
 /*
 Given a positive integer `n`. Print the word (hello) in format of heeeello (letter 'e' must be repeated `n` times). Take input from prompt and print the result in alert.
@@ -21,12 +36,20 @@ n = 7 => output: heeeeeeello
 👇
 */
 // [Your code goes here]
-
+let repeat = prompt("Enter the no. of time you want 'e' to be printed");
+alert("h" + "" +"llo");
 /*
 Program to calculate the sum of first n natural numbers(1,2,3...n are known as natural numbers). Prompt user to enter n (using `prompt`) then based on input provided calculate and show result in `alert`.
 👇
 */
 // [Your code goes here]
+let sum = 10;
+for (i=1; i<=10; i++) {
+  sum += i;
+}
+console.log(sum)
+
+
 
 /* Switch Statement
 
@@ -46,7 +69,39 @@ Take a number value from user and alert the message if it matches the conditions
 
 */
 // [Your code goes here]
+let number = +prompt("Enter a number between 1 to 10");
+switch (number) {
+  case 1 :
+    alert("ONE");
+    break;
+  case 2 :
+    alert("TWO");
+    break;
+  case 3 :
+    alert("THREE");
+    break;
+  case 4 :
+    alert("FOUR");
+    break;
+  case 5 :
+    alert("FIVE");
+    break;
+  case 6 :
+    alert("SIX");
+    break;
+  case 7 :
+    alert("SEVEN");
+    break;
+  case 8 :
+    alert("EIGHT");
+    break;
+  case 9 :
+    alert("NINE");
+    break;
+  default :
+    alert("please enter a value between 1 to 10")
 
+}
 /*
 🎖Using switch statement do the following
 
@@ -62,12 +117,47 @@ Take the value of `marks` (0-100) from user using `prompt` and `alert` the messa
 */
 
 // [Your code goes here]
-
+let marks = prompt("Enter your marks between 0 to 100 to get grade");
+switch(true) {
+  case marks>90 :
+    alert("Grade AA");
+    break;
+  case (marks>80 && marks<=90) :
+    alert("Grade AB");
+    break;
+  case (marks>70 && marks<=80) :
+    alert("Grade BB");
+    break;
+  case (marks>60 && marks<=70) :
+    alert("Grade BC");
+    break; 
+  case (marks>50 && marks<=60) :
+    alert("Grade CC");
+    break;
+  case (marks>40 && marks<=50) :
+    alert("Grade CD");
+    break;
+  case (marks>30 && marks<=40) :
+    alert("Grade DD");
+    break;
+  case(marks<=30) :
+    alert("Grade FF");
+    break;
+  default:
+    alert("invalid input");
+}
 /*
  🎖Write a JavaScript program that takes two `integers` from user (using prompt) and alerts the larger number.
 */
 // [your code goes here]
-
+let int1 = prompt("Enter the first no.");
+let int2 = prompt("Enter the second no.");
+if (int1 > int2) {
+  alert("Larger no. is "+ int1);
+}
+else if (int2 > int1) {
+  alert("Larger no. is "+ int2);
+}
 /*
 🎖Write a JavaScript conditional statement to find the sign (+, -) of product of three numbers. Take those three numbers from user using `prompt`. Display an alert box with the specified sign.
 */
@@ -86,3 +176,25 @@ Take the value of `marks` (0-100) from user using `prompt` and `alert` the messa
 */
 
 // [Your code goes here]
+let number1 = +prompt("Enter 1st number: ");
+let number2 = +prompt("Enter 2nd number : ");
+let sign = prompt("Pick the option among '+' '-' '*' '/' : ");
+if (sign == "+") {
+  alert("the result is "+ (number1+number2));
+}
+else if(sign == "-" && number1>number2) {
+  alert("the result is "+ (number1-number2));
+}
+else if(sign == "-" && number1<number2) {
+  alert("2nd no. is greater than 1st no.");
+}
+else if (sign == "*") {
+  alert("the result is "+ (number1*number2));
+}
+else if(sign == "/" && number1>number2) {
+  alert("the result is "+ (number1/number2));
+}
+else if(sign == "/" && number1<number2) {
+  alert("2nd no. is greater than 1st no.");
+}
+  
